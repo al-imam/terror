90994d3d8162972fbf532918fc0ee846cea8c3d8
al-imam <alimam01828@gmail.com>
2024-01-04 02:13:53 +0600

    add social links

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { GameStatics } from "@/components/game-statics"
 import { Hero } from "@/components/hero"
+import { Links } from "@/components/links"
 
 export default function IndexPage() {
   return (
     <section className="content relative">
       <Hero />
+      <Links />
       <GameStatics />
     </section>
   )
---
+++ components/links.tsx
@@ -0,0 +1,45 @@
+export function Links() {
+  return (
+    <section className="pb-20 pt-24 sm:pb-36 sm:pt-16">
+      <ul className="mx-auto flex max-w-3xl flex-wrap justify-between gap-2 sm:gap-4">
+        <li className="icon icon--twitter">
+          <a href="#" target="_blank">
+            <span className="icon__name">Twitter</span>
+          </a>
+        </li>
+        <li className="icon icon--instagram">
+          <a
+            href="https://www.instagram.com/srabon_exe"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="icon__name">Instagram</span>
+          </a>
+        </li>
+        <li className="icon icon--spotify">
+          <a href="#" target="_blank">
+            <span className="icon__name">Spotify</span>
+          </a>
+        </li>
+        <li className="icon icon--facebook">
+          <a
+            href="https://www.facebook.com/profile.php?id=100075086740578"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="icon__name">Facebook</span>
+          </a>
+        </li>
+        <li className="icon icon--tiktok">
+          <a
+            href="https://www.tiktok.com/@srabon_exe"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="icon__name">Tiktok</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
